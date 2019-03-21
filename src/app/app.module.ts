9ac0629a579e35dd47941ee2c3c20e09ac372a9f
Mohamed Essam <ana3ssam2017@gmail.com>
2019-03-21 16:39:22 +0200

    #16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { from } from 'rxjs';
 import { MyDataComponent } from './my-data/my-data.component';
 import { AssignmentComponent } from './assignment/assignment.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { HeaderComponent } from './header/header.component';
     ServerComponent,
     MyDataComponent,
     AssignmentComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
