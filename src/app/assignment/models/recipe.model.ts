878a82e3fa1f3354c2f2fa2881e4eca63ab6636e
Mohamed Essam <ana3ssam2017@gmail.com>
2019-03-24 13:01:17 +0200

    #18

---
+++ src/app/assignment/models/recipe.model.ts
@@ -0,0 +1,38 @@
+export class Recipe{
+    private name:string;
+    private imagePath:string;
+    private description:string
+ 
+    public setName (name : string) {
+        this.name = name;
+    }
+
+    public setImagePath (imagePath : string) {
+        this.imagePath = imagePath;
+    }
+
+    public setDescription (description : string) {
+        this.description = description;
+    }
+
+    
+    public getName () : string {
+        return this.description;
+    }
+
+    public getImagePath () : string {
+        return this.name;
+    }
+    
+    public getDescription () : string {
+        return this.description;
+    }
+
+    constructor ();
+    
+    constructor (name?:string,description?:string,imagePath?:string){
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+}
